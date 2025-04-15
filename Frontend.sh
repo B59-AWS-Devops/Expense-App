f5b98f8c591d81a6c346e2bca3215bf8ae8a3be6
pavanraju13 <pavanraju4513@gmail.com>
2025-04-15 20:40:50 +0530

    setting hostname

--- Frontend.sh
+++ Frontend.sh
@@ -3,7 +3,7 @@
 # --- FRONTEND SETUP SCRIPT ---
 
 # Update system and install Nginx
-echo -e "\n\tInstalling Nginx : "
+echo -n "Installing Nginx : "
 dnf install nginx -y &>> /tmp/frontend.log
 if [ $? -eq 0 ]; then
     echo -e "\e[32mNginx installed successfully\e[0m"
@@ -25,44 +25,73 @@ fi
 
 
 # Check if nginx is running
+echo -n "Checking Nginx status : "
 systemctl status nginx
 curl -I http://localhost
 
 # Clear existing default web page content
+echo -n "Clearing default web page content : "
 rm -rf /usr/share/nginx/html/*
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mCleared default web page content successfully\e[0m"
+    exit 0
+else
+    echo -e "\e[31mFailed to clear default web page content. Check /tmp/frontend.log for details.\e[0m"
+    exit 1
+fi
 
 # Download frontend zip
-echo "Downloading frontend..."
+echo -n "Downloading frontend..."
 curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mFrontend zip downloaded successfully\e[0m"
+else
+    echo -e "\e[31mFailed to download frontend zip. Check /tmp/frontend.log for details.\e[0m"
+    exit 1
+fi
 
 # Unzip frontend to nginx html directory
+echo -n "Unzipping frontend to nginx html directory..."
 cd /usr/share/nginx/html
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mChanged directory to /usr/share/nginx/html successfully\e[0m"
+else
+    echo -e "\e[31mFailed to change directory. Check /tmp/frontend.log for details.\e[0m"
+    exit 1
+fi
 #check unzip is installed
 
-unzip /tmp/frontend.zip &>> /tmp/frontend.log
+unzip -o /tmp/frontend.zip &>> /tmp/frontend.log
+if [ $? -eq 0 ]; then
+    echo -e "\e[32mFrontend unzipped successfully\e[0m"
+else
+    echo -e "\e[31mFailed to unzip frontend. Check /tmp/frontend.log for details.\e[0m"
+    exit 1
+fi
 
-# Create Nginx reverse proxy configuration
-cat <<EOF > /etc/nginx/default.d/expense.conf
-proxy_http_version 1.1;
-
-location /api/ {
-    proxy_pass http://localhost:8080/;
-}
-
-location /health {
-    stub_status on;
-    access_log off;
-}
-EOF
 
 # Restart nginx to apply changes
-echo "Restarting nginx..."
+echo -n "Restarting nginx..."
 systemctl restart nginx
+if  [ $? -eq 0 ]; then
+    echo -e "\e[32mNginx restarted successfully\e[0m"
+else
+    echo -e "\e[31mFailed to restart Nginx. Check /tmp/frontend.log for details.\e[0m"
+    exit 1
+fi
 
 # Confirm nginx status
+echo -n "Confirming Nginx status..."
 systemctl status nginx
+if  [ $? -eq 0 ]; then
+    echo -e "\e[32mNginx is running\e[0m"
+else
+    echo -e "\e[31mNginx is not running. Check /tmp/frontend.log for details.\e[0m"
+    exit 1
+fi
 
 # Optional: Set hostname
 hostnamectl set-hostname Frontend
+
 
 echo "Frontend setup completed successfully."
