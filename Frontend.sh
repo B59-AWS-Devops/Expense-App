417cbabffde135208acac8774fc46f100b4dd8b5
pavanraju13 <pavanraju4513@gmail.com>
2025-04-15 20:01:18 +0530

    setting hostname

--- Frontend.sh
+++ Frontend.sh
@@ -3,12 +3,25 @@
 # --- FRONTEND SETUP SCRIPT ---
 
 # Update system and install Nginx
-echo "Installing Nginx..."
+echo -e "Installing Nginx..."
 dnf install nginx -y &>> /tmp/frontend.log
+if [ $? -eq 0 ]; then
+    echo "Nginx installed successfully."
+else
+    echo "Failed to install Nginx. Check /tmp/frontend.log for details."
+    exit 1
+fi
 
 # Enable and start nginx
+echo -e "Starting Nginx.."
 systemctl enable nginx
 systemctl start nginx
+if [ $? -eq 0 ]; then
+    echo -e "Nginx Started \e[32m successfully \e [0m"
+else
+    echo "Failed to start Nginx. Check /tmp/frontend.log for details."
+    exit 1
+fi
 
 # Check if nginx is running
 systemctl status nginx
@@ -23,6 +36,8 @@ curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
 
 # Unzip frontend to nginx html directory
 cd /usr/share/nginx/html
+#check unzip is installed
+
 unzip /tmp/frontend.zip &>> /tmp/frontend.log
 
 # Create Nginx reverse proxy configuration
