55a245b583ddb9a2d2babfd023ff04eb6bd568c4
pavanraju13 <pavanraju4513@gmail.com>
2025-04-16 10:18:01 +0530

    setting hostname

--- Frontend.sh
+++ Frontend.sh
@@ -7,6 +7,15 @@
 component=frontend
 log_file=/tmp/$component.log
 
+VALIDATE () {
+    if [ $(id -u) -eq 0 ]; then
+        echo -e "\e[32mUser is root\e[0m"
+    else
+        echo -e User is not root. Please run as root."\e[31m sudo sh $0\e[0m"
+        exit 2
+    fi
+}
+
 stat () {
 if [ $1 -eq 0 ]; then
     echo -e "\e[32msuccessfully\e[0m"
